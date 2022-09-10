058f96d557bc97e70bebf2bb7b526b0858bdb071
Jason Balladares <jsonballadares@gmail.com>
2022-09-10 00:54:19 -0400

    added theme and set up theme

--- swindle-savers/src/index.js
+++ swindle-savers/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App/App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
     <App />
+  </ThemeProvider>,
   </React.StrictMode>
 );
 
