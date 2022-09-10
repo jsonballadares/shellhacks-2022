0173cc0db5673a7f60dd83ba778e98d3100a86f1
Jason Balladares <jsonballadares@gmail.com>
2022-09-10 15:46:30 -0400

    added first three pages

--- swindle-savers/src/appRoutes.js
+++ swindle-savers/src/appRoutes.js
@@ -3,18 +3,42 @@ import { Navigate, RouteProps } from 'react-router-dom';
 import { AppBar } from '@mui/material';
 import DrawerAppBar from './Components/DrawerAppBar/DrawerAppBar';
 import ClippedDrawer from './Components/ClippedDrawer/ClippedDrawer';
+import SwindlesPage from './Components/SwindlesPage/SwindlesPage';
+import AlertsPage from './Components/AlertsPage/AlertsPage';
+import AboutPage from './Components/AboutPage/AboutPage';
 
 const appRoutes = [
   {
-    path: '/dashboard',
+    path: '/',
     element: (
-        <ClippedDrawer></ClippedDrawer>
+        <ClippedDrawer>
+        </ClippedDrawer>
     ),
   },
   {
-    path: '/',
-    element: <Navigate to="/dashboard" />,
-  }
+    path: '/swindles',
+    element: (
+        <ClippedDrawer>
+            <SwindlesPage></SwindlesPage>
+        </ClippedDrawer>
+    ),
+  },
+  {
+    path: '/alerts',
+    element: (
+        <ClippedDrawer>
+            <AlertsPage></AlertsPage>
+        </ClippedDrawer>
+    ),
+  },
+  {
+    path: '/about',
+    element: (
+        <ClippedDrawer>
+            <AboutPage></AboutPage>
+        </ClippedDrawer>
+    ),
+  },
   
 ];
 
